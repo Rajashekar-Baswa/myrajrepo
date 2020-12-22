3c90103d6d3d39fc2e53b88164aa21e71a69d635
Rajashekar Baswa <baswaraj464@gmail.com>
2020-12-21 21:37:20 -0600

    added space for deployment 3 in jenkins

--- src/main/java/com/raj/rajspringboot/App.java
+++ src/main/java/com/raj/rajspringboot/App.java
@@ -2,6 +2,7 @@ package com.raj.rajspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan("com.raj.rajspringboot")
 @SpringBootApplication
 
-public class App 
+public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
